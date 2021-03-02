1d09c020672f229aa5e53a4c4a7bfa6272878115
Alex112292 <57815789+Alex112292@users.noreply.github.com>
2021-03-02 13:32:55 -0500

    Made message meaner

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -27,7 +27,7 @@ public class DmvSimulator {
 			System.out.println(count);
 
 		}
-		System.out.println("Sorry you do not have the correct paperwork. Go to the back of the line please!");
+		System.out.println("You do not have the correct paperwork. Go to the back of the line.");
 
 	}
 }
