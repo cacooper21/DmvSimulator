6ebd9530413e12f339b7ceae9e0b4c5981223757
cacooper21 <58480492+cacooper21@users.noreply.github.com>
2021-03-02 16:57:06 -0500

    please work

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -15,7 +15,7 @@ public class DmvSimulator {
 
 			if (count == 201) {
 				i = 0;
-				
+
 			}
 
 			if (count == Usernum) {
@@ -27,13 +27,13 @@ public class DmvSimulator {
 			System.out.println(count);
 
 		}
-		
+
 		int Usernum2 = (int) (Math.random() * 100);
-		
+
 		if (Usernum2 == 1) {
-		System.out.println("You have the correct paperwork. You are all set.");
+			System.out.println("You have the correct paperwork. You are all set.");
 		}
-		
+
 		else {
 			System.out.println("You do not have the correct paperwork. Go to the back of the line. muahahahahah!");
 		}
